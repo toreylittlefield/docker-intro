34d8cdd2f5c2ee8ca31ae6953e1b36dbdeea967e
toreylittlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-10-11 02:11:33 -0400

    first node server in JS

---
+++ docker-first-node-server/index.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+http
+  .createServer(function(request, response) {
+    console.log("request received", request);
+    response.end("omg hi", "utf-8");
+  })
+  .listen(3000);
+console.log("server started");
